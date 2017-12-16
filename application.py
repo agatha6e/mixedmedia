b4eabaf9c6e65417f80851d76989042738fe87b3
Agatha Sunyoung Park <sunyoungpark@gsd.harvard.edu>
2017-12-16 03:08:16 +0000

    First commit

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request, session, jsonify
 from flask_session import Session
+from tempfile import mkdtemp
 
 
 import requests
@@ -8,6 +9,20 @@ import json
 
 # Configure application
 app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route("/articles_left")
