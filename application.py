19c5f871e2ceeae844bacbd766839014734d91b9
Agatha Sunyoung Park <sunyoungpark@gsd.harvard.edu>
2017-12-16 02:08:03 +0000

    First commit

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
+from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session, jsonify
 from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
 
 import requests
 import json
@@ -17,14 +20,16 @@ def after_request(response):
 
 
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///mixedmedia.db")
 
 
 @app.route("/articles_left")
