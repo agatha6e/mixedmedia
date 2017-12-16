284dba3de58b3664f0b9adaa8f420c9bffb5c3d4
Agatha Sunyoung Park <sunyoungpark@gsd.harvard.edu>
2017-12-16 03:42:01 +0000

    First commit

--- application.py
+++ application.py
@@ -10,13 +10,6 @@ import json
 # Configure application
 app = Flask(__name__)
 
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
